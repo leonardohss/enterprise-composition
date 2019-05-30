8e082197cf17f503475400731d0ae9aa0c5b8e8d
Leonardo Sousa <leorick97@gmail.com>
2019-05-29 22:03:45 -0300

    Criação do projeto

---
+++ EnterpriseComposition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnterpriseComposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
