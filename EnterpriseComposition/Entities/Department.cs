e03e3535e71796fbe174381b61176466dfbcab7a
Leonardo Sousa <leorick97@gmail.com>
2019-05-29 22:12:28 -0300

    Implementation of Department class

---
+++ EnterpriseComposition/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnterpriseComposition.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
