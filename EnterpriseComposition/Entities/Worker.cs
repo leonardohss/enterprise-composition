6f068b222f45378091aa6c33bb771b2ffdb27f4f
Leonardo Sousa <leorick97@gmail.com>
2019-05-29 22:27:45 -0300

    Create Worker class

--- EnterpriseComposition/Entities/Worker.cs
+++ EnterpriseComposition/Entities/Worker.cs
@@ -1,10 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using EnterpriseComposition.Entities.Enums;
 
 namespace EnterpriseComposition.Entities
 {
     class Worker
     {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+
+
     }
 }
