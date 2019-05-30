600d3fb7061194ffca73d39a28ce2ee89cfa5031
Leonardo Sousa <leorick97@gmail.com>
2019-05-29 22:36:11 -0300

    Continuation Worker class

--- EnterpriseComposition/Entities/Worker.cs
+++ EnterpriseComposition/Entities/Worker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using EnterpriseComposition.Entities.Enums;
+using System.Collections.Generic;
 
 namespace EnterpriseComposition.Entities
 {
@@ -9,7 +10,17 @@ namespace EnterpriseComposition.Entities
         public WorkerLevel Level { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+        //propriedade que representa que o trabalhador tem varios contratos
 
+        public Worker() { }
 
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
     }
 }
